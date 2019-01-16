c09e8c46c81554f0dad198352c6b24003d512262
16CO139 <16co139@example.com>
2019-01-16 17:04:17 +0530

    FSFC , SRTF

--- lab3/FCFS.cpp
+++ lab3/FCFS.cpp
@@ -8,51 +8,53 @@ void printall(int n, vector< pair<int,int> > arrivalBurst)
 {
 	sort(arrivalBurst.begin(),arrivalBurst.end());
 	int wait[n],completion[n],wait1[n];
-	completion[0] = arrivalBurst[0].second;
+	completion[0] = 0;
 	int tat[n];
 	wait1[0] = arrivalBurst[0].first;
 	tat[0]=arrivalBurst[0].second;
-	int twt=0,ttat=tat[0];
-	completion[0] = arrivalBurst[0].second;
-	f(i,1,n)
+	float twt=0,ttat=0;
+	f(i,1,n+1)
 	{	
-		if(arrivalBurst[i].first <= completion[i-1])
-		completion[i] = completion[i-1] + arrivalBurst[i].second;
-		else 
-			completion[i] = completion[i-1] + arrivalBurst[i].first +arrivalBurst[i].second;
+		completion[i] = completion[i-1]+arrivalBurst[i].second;
+		if(arrivalBurst[i].first>completion[i-1])
+			completion[i]+=arrivalBurst[i].first-completion[i-1];
 		tat[i] = completion[i]-arrivalBurst[i].first;
 		wait1[i] = tat[i]-arrivalBurst[i].second;
 		ttat += tat[i];
 		twt += wait1[i];
 
 	}
-	ttat /=n;
-	twt /=n;
+	ttat =(float)ttat/n;
+	twt =(float)twt/n;
 	cout<<"process no\t"<<"arrival no\t"<<"burst time\t"<<"completion time\t\t"<<"turn around time\t"<<"waiting time\n";	
 	
-	f(i,0,n)
-		cout<<i<<"\t\t"<<arrivalBurst[i].first<<"\t\t"<<arrivalBurst[i].second<<"\t\t"<<completion[i]<<"\t\t\t"<<tat[i]<<"\t\t\t"<<wait1[i]<<"\n";
+	for(int i=1;i<=n;++i)
+	{
+		if(arrivalBurst[i].first>completion[i-1])
+			printf("ideal\n");
+			cout<<i<<"\t\t"<<arrivalBurst[i].first<<"\t\t"<<arrivalBurst[i].second<<"\t\t"<<completion[i]<<"\t\t\t"<<tat[i]<<"\t\t\t"<<wait1[i]<<"\n";
+	}
 	cout<<"average waiting time is:"<<twt<<"\n average turn around time is:"<<ttat<<"\n";
 }
 int main()
 {
-	int n,temp1,temp2;
+	int n,arrival,burst;
 	cout<<"Enter number of processes:";
 	cin>>n;
-	int processes[n];
-	f(i,0,n)
-	{
-		processes[i] = i+1;
-	}
+	// int processes[n];
+	// f(i,0,n)
+	// {
+	// 	processes[i] = i+1;
+	// }
 	cout<<"Enter arrival time and burst time\n";
-	int burst[n];
-	int arrival[n];
-	vector< pair<int ,int> > arrivalBurst(n);
-	f(i,0,n)
+	// int burst[n];
+	// int arrival[n];
+	vector< pair<int ,int> > arrivalBurst(n+1);
+	f(i,1,n+1)
 	{
-		cin>>temp1>>temp2;
-		arrivalBurst[i].first = temp1;
-		arrivalBurst[i].second = temp2;
+		cin>>arrival>>burst;
+		arrivalBurst[i].first = arrival;
+		arrivalBurst[i].second = burst;
 	}
 	printall(n,arrivalBurst);
 }
